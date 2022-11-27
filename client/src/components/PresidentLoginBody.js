49082159067e5ee8877e9449863d12070d60ce7f
princid <princid85@gmail.com>
2022-11-27 16:10:57 +0530

    Updated the President Login body

--- client/src/components/PresidentLoginBody.js
+++ client/src/components/PresidentLoginBody.js
@@ -1,14 +1,19 @@
 // import { Link } from "@nextui-org/react";
 import React, {useContext, useState} from "react";
 import {Navigate} from "react-router-dom";
+import { message } from "antd";
+
+// import "./App.css";
 import "./Login.css";
 import UserContext from "../UserContext";
 import {loginPresident} from "../api";
 import {useNavigate} from "react-router";
 
 
-export const PresidentLoginBody = () => {
+export const PresidentLoginBody = ({ callback }) => {
   const { user } = useContext(UserContext);
+
+  console.log(user)
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -21,9 +26,13 @@ export const PresidentLoginBody = () => {
     formDetails.append('password', password);
     loginPresident(formDetails).then((res) => {
       if(res.status === 200) {
-        navigate('/')
+        callback();
+        message.success(res.data.message);
+        navigate(`/${res.data.user.club}`)
       }
-    }).catch(console.log);
+    }).catch((err) => {
+      message.error(err?.response?.data?.message || 'Something went wrong!');
+    });
   }
 
   if(user.isAuth) {
@@ -33,24 +42,38 @@ export const PresidentLoginBody = () => {
 
   return (
     <div className="Login__form">
-      <h2>President Login</h2>
+      {/* 2nd page */}
+      <h2>PRESIDENT LOGIN</h2>
       <div className="container" id="container">
         <div className="form-container sign-in-container">
           <div className="form">
             <h1 className="h1__text">Log in</h1>
-            <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)}/>
-            <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)}/>
-            <br/>
-            <button className="Login__btn" onClick={handleSubmit}>Log In</button>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <br />
+            <button className="Login__btn" onClick={handleSubmit}>
+              Log In
+            </button>
           </div>
         </div>
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-right">
-              <h1>Hey, there!</h1>
+              <h1 className="hey">Hey, there!</h1>
               <p>To login as Admin, Click below.</p>
               <a href="/adminlogin">
-                <button className="Login__btn"
+                <button
+                  className="Login__btn"
                   // class="ghost"
                   type="submit"
                   id="president_login"
