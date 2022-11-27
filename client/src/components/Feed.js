2a961bb89ed8df6c265ac54dae38272b5774f654
princid <princid85@gmail.com>
2022-11-27 16:06:34 +0530

    updated the feed

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,40 +1,44 @@
-import React, {useEffect, useState} from 'react'
-import CreatePost from './CreatePost'
-import Post from './Post'
-import {getAllClubEvents} from "../api";
-import './Feed.css'
+import React, {useContext, useEffect, useState} from "react";
+import CreatePost from "./CreatePost";
+import Post from "./Post";
+import { getAllClubEvents } from "../api";
+import "./Feed.css";
+import UserContext from "../UserContext";
 
+const Feed = ({ clubId }) => {
+  const { user } = useContext(UserContext);
 
-const Feed = ({clubId, isPresident}) => {
-  const [events, setEvents] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const isPresident = user.isAuth && user.type === "president" && user.club === clubId;
+
+  const fetchAllEvents = () => {
+    setIsLoading(true);
     getAllClubEvents(clubId)
       .then(({data}) => {
-        setEvents(data.data)
-        setIsLoading(false)
+        setEvents(data.data);
+        setIsLoading(false);
       })
-      .catch(err => console.log(err))
-  }, [clubId])
+      .catch((err) => console.log(err));
+  }
 
+  useEffect(() => {
+    fetchAllEvents();
+  }, [clubId]);
 
   const posts = events.map(({ _id, title, imageURL, date, addedBy }) => (
     <Post
       profilePic="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
-      message={title}
-      timestamp={new Date(date).getTime()}
-      imageURL={imageURL}
-      username={addedBy}
-      isPresident={isPresident}
-      id={_id}
+      title={title} timestamp={new Date(date).getTime()} imageURL={imageURL} username={addedBy}
+      isPresident={isPresident} id={_id} callback={fetchAllEvents}
     />
   ));
 
   return (
     <div className="feed">
-      {isPresident && <CreatePost clubId={clubId} className='create-post'/>}
-      {isLoading ? (<div>Events are getting loaded!</div>) : (<div className='post'>{posts}</div>)}
+      {isPresident && <CreatePost clubId={clubId} className="create-post" callback={fetchAllEvents}/>}
+      {isLoading ? (<div>Events are getting loaded!</div>) : (<div className="posts">{posts}</div>)}
     </div>
   );
 };
